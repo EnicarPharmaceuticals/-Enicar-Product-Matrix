658ae1f8324fbb820a4e7c97328e4b36ff9c0f4d
Pharchem <info@pharchem.in>
2026-08-22 08:39:25 +0000

    Update matrix

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Public. Writes are authorised by the RA/QA password, checked
 // server-side by Apps Script. BOM data is encrypted - SECURITY.md.
 const APP_CONFIG = {
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxWX8iGDlW-A9mLdgPZZa6aJq2dPix0bmmDCP_XalQloIohUQlpE7_YlsJq48UEhgw-/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxBvxp4bg19JTZHJP9tuuGP86BQZ8X_2vhE2QIUuqGS1Mx-NVVsp186DHCN_nxURZvF/exec",
 };
